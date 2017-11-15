10f5a48dfbf1389acf77425f1540a3f6589b386d
Chris Holdgraf <choldgraf@berkeley.edu>
2017-11-15 15:54:11 -0500

    renaming and supporting folders

--- gitautopush/gitautopush.py
+++ gitautopush/gitautopush.py
@@ -6,33 +6,34 @@ import argparse
 import shutil as sh
 from datetime import datetime
 
-DESCRIPTION = ("Automatically syncronize and push a file to GitHub.\n"
-               "Useful for teaching with a file that you populate "
+DESCRIPTION = ("Automatically syncronize and push a file/foder to GitHub.\n"
+               "Useful for teaching with one of more files that you populate "
                "as you go along. This tool will automatically sync "
                "changes that you make and push them to GitHub so that "
-               "students can use it as a reference")
+               "students can use it as a reference.")
 parser = argparse.ArgumentParser(description=DESCRIPTION)
-parser.add_argument("path", help="Path to the notebook to be pushed.")
+parser.add_argument("path", help="Path to the notebook or folder to be pushed.")
 parser.add_argument("--sleep", default=15, help="Time to wait (in seconds) before checking for updates.")
-   
-    
+parser.add_argument("--rename", default=None, help="A new name for the file/folder to be pushed to GitHub.")
+
 def main():
     args = parser.parse_args()
 
     path = args.path
     this_sleep = int(args.sleep)
-    
-    name = os.path.basename(path)
-    new_path = os.path.join('.', name)
+    rename = str(args.rename) if args.rename is not None else None
+    newname = os.path.basename(path) if rename is None else rename
+    new_path = os.path.join('.', newname)
     if not os.path.exists(path):
-        raise ValueError("Notebook doesn't exist, double-check your path")
+        raise ValueError("Path doesn't exist, double-check your path")
 
+    copy = sh.copytree if os.path.isdir(path) else sh.copy
     if not os.path.exists(new_path):
-        print('Adding new file: {}'.format(new_path))
-        sh.copy(path, new_path)
+        print('Adding new path: {}'.format(new_path))
+        copy(path, new_path)
         call(['git', 'add', new_path])
-        call(['git', 'commit', '-m', '"new file: {}"'.format(name)])
-    
+        call(['git', 'commit', '-m', '"new path: {}"'.format(newname)])
+
     ii = 1
     while True:
         sh.copy(path, new_path)
@@ -44,6 +45,6 @@ def main():
             ii += 1
         sleep(this_sleep)
 
-    
+
 if __name__ == '__main__':
     main()
